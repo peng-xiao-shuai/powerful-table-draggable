4a79f901fe0b98f81a0c646ba17b1d4fe2f9e394
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-03 10:35:22 +0800

    style: dict file code optimization

--- src/modules/dict.ts
+++ src/modules/dict.ts
@@ -2,52 +2,27 @@
  * 字典
  */
 
+/**
+ * 转换options
+ * @param labels label名称数组
+ * @param values 数据数组
+ * @returns {label: string, value: any}
+ */
+const optionsFun = (labels: string[], values?: (number|string|boolean|undefined)[]) => labels.map((item, index) => ({ label: item, value: values ? values[index] : item }))
+
 // 排序
-export const sortableOptions = [{
-  label: '排序',
-  value: true
-},{
-  label: '不排序',
-  value: false
-},{
-  label: '自定义',
-  value: 'custom'
-}]
+export const sortableOptions = optionsFun(['排序', '不排序', '自定义'], [true, false, 'custom'])
 // 固定
-export const fixedOptions = [{
-  label: '不固定',
-  value: false
-},{
-  label: '固定左侧',
-  value: 'left'
-},{
-  label: '固定右侧',
-  value: 'right'
-}]
+export const fixedOptions = optionsFun(['不固定', '固定左侧', '固定右侧'], [false, 'left', 'right'])
 // 显示提示
-export const overflowTooltipOption = [{
-  label: '显示提示',
-  value: true
-},{
-  label: '不显示提示',
-  value: false
-}]
+export const overflowTooltipOption = optionsFun(['显示提示', '不显示提示'], [true, false])
 // 输入框插槽方向
-export const inputSlotDirection = [{
-  label: '前面',
-  value: 'prepend'
-},{
-  label: '后面',
-  value: 'append'
-},{
-  label: '不显示插槽',
-  value: undefined
-}]
+export const inputSlotDirection = optionsFun(['前面', '后面', '不显示插槽'], ['prepend', 'append', undefined])
 // 禁用
-export const disabledOptions = [{
-  label: '禁用',
-  value: true
-},{
-  label: '不禁用',
-  value: false
-}]
+export const disabledOptions = optionsFun(['禁用', '不禁用'], [true, false])
+// 对齐方式
+export const alignOptions = optionsFun(['居左', '居中', '居右'], ['left', 'center', 'right'])
+// 主题
+export const themeOptions = optionsFun(['默认', '成功', '警告', '危险', '信息', '文本'], ['primary', 'success', 'warning', 'danger', 'info', 'text'])
+// 图片填充方式
+export const imageFitOption = optionsFun(['fill', 'contain', 'cover', 'none', 'scale-down'])
