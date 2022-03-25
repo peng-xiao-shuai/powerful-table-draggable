188ea53419993f5499cc819ea30fce589a60fd4d
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-25 10:52:14 +0800

    chore: Update dependency package el-plus-powerful table-ts.  Delete some files because el-Plus...  It contains

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import App from "./App.vue"
 
 import ElementPlus from "element-plus"
 // import powerfulTable from "../lib/powerful-table.umd.min.js"
-import powerfulTable from "./powerfulTable/index"
-// import powerfulTable from 'el-plus-powerful-table-ts'
+import powerfulTable from 'el-plus-powerful-table-ts'
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import "element-plus/theme-chalk/src/index.scss"
 import './powerfulTable/index.scss';
