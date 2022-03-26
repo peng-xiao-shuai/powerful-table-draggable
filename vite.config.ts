436210ab6560925d0c57432d00488c69ac88c106
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-26 12:45:01 +0800

    fix: path error

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "#": path.resolve(__dirname, "./type")
+      "#": path.resolve(__dirname, "./types")
     },
   },
   server: {
