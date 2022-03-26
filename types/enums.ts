0f8885753cffe2e1137c4964f236f4b5bae5faf9
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-26 12:44:38 +0800

    types: added enums file

---
+++ types/enums.ts
@@ -0,0 +1,22 @@
+export enum GroupName {
+  Layout = 'layout',
+  People = 'people'
+}
+
+export enum BasicsComponentType {
+  Image = 'image',
+  Btn = 'btn',
+  Switch = 'switch',
+  Input = 'input',
+  Text = 'text',
+  Textarea = 'textarea',
+  Iconfont = 'iconfont',
+  Tag = 'tag',
+  Rate = 'rate',
+  Link = 'link',
+  Video = 'video'
+}
+
+export enum OtherComponentsType {
+  Layout = 'layout'
+}
