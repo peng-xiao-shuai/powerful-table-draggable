b112eeb7c7fcb2148955631cec2271d6d015bc89
Peng_Xiao_Shuai_0902 <1612565136@qq.com>
2022-03-26 12:45:44 +0800

    style: import external style file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue"
 import ElementPlus from "element-plus"
 // import powerfulTable from "../lib/powerful-table.umd.min.js"
 import powerfulTable from 'el-plus-powerful-table-ts'
+import 'el-plus-powerful-table-ts/lib/index.min.css';
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import "element-plus/theme-chalk/src/index.scss"
 import './powerfulTable/index.scss';
